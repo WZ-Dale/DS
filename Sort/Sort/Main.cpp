ce8c06a7b0ec9c8e6687016c5d8343c04676d0c5
wangze <w18829898295@163.com>
2019-04-24 12:10:36 +0800

    排序测试

---
+++ Sort/Sort/Main.cpp
@@ -0,0 +1,16 @@
+#include "BasicSort.h"
+#include "ShellSort.h"
+#include "stdlib.h"
+#include "Time.h"
+#include "QuickSort.h"
+#include "MergeSort.h"
+
+int main(){
+	//Test();
+	//Test1();
+	//Time();
+	//Test2();
+	Test3();
+	system("pause");
+	return 0;
+}
